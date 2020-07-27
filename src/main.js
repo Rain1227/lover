2bdfec0aa39761899ef8d310f60bd42d218d3894
Rain1227 <1262208607@qq.com>
2020-07-27 18:34:20 +0800

    realy

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css"; // 引入样式
+import "./style/index.less";
 
-Vue.config.productionTip = false
-Vue.use(Element)
+Vue.config.productionTip = false;
+Vue.use(Element);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
